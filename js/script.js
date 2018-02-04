000af40f991075d49815a6097fecb302640862af
hasstMac <hasstmac@hasstMacs-MacBook-Air.local>
2018-02-03 23:17:43 -0800

    created and fiXeD typ0s, got canvas on index, observed objects, ate orange

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 console.log("js is here");
+
 // Tim Hass
 // Feb 2018
 // GA - Project 1
@@ -16,25 +17,27 @@ console.log("js is here");
 // the game is timed every 2 minutes the game gets a point
 // the game moves to the next level when PlayerX reaches 10 points (timers reset for each level)
 
+// player must move around all obstacles in the game
+//    if player hits the same obstable twice
+//        while poop in hand the poop will drop
+// player must pick up poop again
 // player must move to the poop safeZone
 // player must click on poop object to collect it (clicks vary per level)
-// if player collides with a single poop object the game gets 5 points
-// player can not collect poop that has been stepped in
-// stepped in poop becomes an obstable (but can be stepped in again)
-// if stepped in =2 the poop is destroyed and game gets 5 points
-// player must move to trashCan and click to dispose (one click)
-// player must move around obstacles
-// if player hits the same obstable twice while poop in hand the poop will drop
-// player must pick up poop again
+//    if player collides with a single poop object
+//        the stepped in poop becomes an obstable (but can be stepped in again)
+//    if stepped in =2 the poop is destroyed and game gets 5 points
+// player must move to trashCan safeZone
+// player must click to dispose (one click)
+// be careful you can full into the trashcan (gameover)
+
 // Brain will run around and play in the area
 // Brain is set on a 30 second timer
+// Brian moves at a set set interal
 // Brain randomly drops poop
 // Brain is redirected when he collides with and obstacle
-// Brian moves at a set set interal
-//
-
 
 // to do
+// resize canvasSize
 // 2 clicks outside of obstacle when not in safeZone
 // 2 clicks on poop collects the poop
 // 1 click on trashCan collects the poop
@@ -44,127 +47,125 @@ console.log("js is here");
 // playBall
 //
 
-
-var dogsInPlay = [];
-var obstaclesInPlay = [];
-var playersInPlay = [];
-
+var GameBoard = {
+  name: "Pooper Scooper",
+  src: "imageLink",
+  winGameScore: 10,
+  gameScore: [],
+  gameTimer: 0,
+  level: {
+    id: 1,
+    speed: 1
+  },
+  trashCanX: {
+    trashCanY: null
+  },
+  dogX: {
+    dogY: []
+  },
+  playerX: {
+    playerArray: []
+  },
+  obstableX: {
+    dogHouseY: null,
+    treeY: [],
+    poopY: []
+  },
+  action: {
+    loadGameBoard: function(){return this.speed},
+    createObstacles: function(){return this.speed},
+    createPlayer: function(){return this.speed},
+    setGamePieceTimeOut: function(){return this.speed},
+    setGamePieceInterval: function(){return this.speed},
+    safeZone: function(){return this.speed},
+    fireOffense: function(){return this.speed},
+    destroyKeyPress: function(){return this.speed},
+    onGameKeyPress: function(){return this.speed},
+    onPoopScoop: function(){return this.speed}
+  }
+}
 var Dog = {
   name: "dogsName",
   speed: GameBoard.level.speed,
-  location: {},
+  ctx: [],
   action: {
     sit: function(){return this.speed},
     run: function(){return this.speed},
-    dropPoop: function(){
-      // return this.speed + this.poop
-      poop: {
-        size: 1,
-        clicks: 0,
-        action: {
-          destroyPoop: function(){return this.clicks - this.size},
-          resetClicks: function(){return this.clicks - this.size},
-          becomeObstacle: function(){return this.clicks - this.size},
-        }
-    },
     bark: function(){return this.speed},
-    redirect: function(){return this.speed}
-  }
-  }
+    redirect: function(){return this.speed},
+    dropPoop: function(){return this.speed + this.poop}
+    }
 }
 var Player = {
-  name: "player" + id,
-  id: "player_",
+  name: "player",
+  id: "player#",
   speed: GameBoard.level.speed,
   level: GameBoard.level.id,
-  location: {},
+  ctx: [],
   score: 0,
   highScore: 0,
   action: {
     walk: function(){return this.speed},
     run: function(){return this.speed},
-    dumpPoop: function(){return this.speed}
-    pickUpPoop: function(){return this.speed}
+    dumpPoop: function(){return this.speed},
+    pickUpPoop: function(){return this.speed},
     stepInPoop: function(){return this.speed}
   }
 }
-var Obstacle =
+var Obstacle = {
   tree: {
     id: "tree",
-    src: "imageLink"
-    location: {},
-    location: [],
+    src: "imageLink",
+    ctx: [],
     level: GameBoard.level.id,
     action: {
       blockMove: function(){return this.speed},
       dropPoop: function(){return this.speed}
+    }
   },
   dogHouse: {
     id: "dogHouse",
     src: "imageLink",
-    location: {},
+    ctx: [],
     level: GameBoard.level.id,
     action: {
       blockMove: function(){return this.speed},
       dropPoop: function(){return this.speed}
+    }
   },
   trashCan: {
     id: "trashCan",
     src: "imageLink",
-    location: {},
+    ctx: [],
     level: GameBoard.level.id,
     full: 10,
     action: {
       checkFull: function(){return this.full, this.level},
       emptyCan: function(){return this.full}
-  }
-}
-var GameBoard = {
-  name: "Pooper Scooper",
-  src: "imageLink",
-  winGameScore: 10,
-  gameScore: 0,
-  gameTimer: 0,
-  screenSize: {},
-  canvasSize: {},
-  canvas: {},
-  level: {
-    id: 1
-    speed: 1
-  }
-  action: {
-    loadGameBoard: function(){return this.speed},
-    createObstacles: function(){return this.speed},
-    createPlayer: function(){return this.speed},
-    setGamePieceTimeOut: function(object){return this.speed},
-    setGamePieceInterval: function(object){return this.speed},
-    safeZone: function(){return this.speed}
-    fireOffense: function(){return this.speed}
-    destroyKeyPress: function(){return this.speed}
-    onGameKeyPress: function(){return this.speed}
-    onMouseClick: function(){return this.speed}
+    }
   },
-  obstableX: {
-    dogHouseX: null,
-    treeX: null
-    poopX: null
-  },
-  poopsX: {
-    poopY: null
-  },
-  player#: {
-
-  },
-  dogX: {
-    dogY: null
-  },
-  trashCanX: {,
-    trashCanY: null
+  poop: {
+    size: 1,
+    scoops: GameBoard.level.id,
+    src: "imageLink",
+    ctx: [],
+    actvity: {
+      destroyPoop: function(){return this.clicks - this.size},
+      resetClicks: function(){return this.clicks - this.size},
+      becomeObstacle: function(){return this.clicks - this.size}
+    }
   }
 }
 
-// var friend = {firstName: "Jane", lastName: "Doe"}
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 $(document).ready(function(){
 
+  canvas.addEventListener("click", function(event) {
+      drawRectangle(ctx, event.offsetX, event.offsetY);
+    });
 });
